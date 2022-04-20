0e8e91a4fbb0b72c423d325579c37eaef255b995
hamzaj1127 <89408188+hamzaj1127@users.noreply.github.com>
2022-04-20 11:47:44 -0400

    Add files via upload

--- 8ball/8ball.py
+++ 8ball/8ball.py
@@ -1,49 +1,36 @@
 import random
 
-QUESTION = input('what do you wanna ask the 8 ball?')
-v10 =random.randint(1,9)
-
-if v10==1:
-    print("Yes - definitely.")
-elif v10==2:
-    print("It is decidedly so.")
-elif v10==3:
-    print("Without a doubt.")
-elif v10==4:
-    print("Reply hazy, try again.")
-elif v10==5:
-    print("Ask again later.")
-elif v10==6:
-    print("Better not tell you now.")
-elif v10==7:
-    print("My sources say no.")
-elif v10==8:
-    print("Outlook not so good.")
-elif v10==9:
-    print("Very doubtful.")
-
-x2 = input('Do you want to ask another question? Yes or no?')
-v11 =random.randint(1,9)
-if x2.lower()==('nah', 'no', 'n'):
-   print('goodbye!')
-
-elif  x2.lower()==('yes', 'yeah', 'y'):
-    input('what do you wanna ask the 8 ball?')  
-if v11==1:
+def eight_ball_responses():
+    QUESTION = input('what do you wanna ask the 8 ball?')
+    v10 =random.randint(1,9)
+    if v10==1:
         print("Yes - definitely.")
-elif v11==2:
+    elif v10==2:
         print("It is decidedly so.")
-elif v11==3:
+    elif v10==3:
         print("Without a doubt.")
-elif v11==4:
+    elif v10==4:
         print("Reply hazy, try again.")
-elif v11==5:
+    elif v10==5:
         print("Ask again later.")
-elif v11==6:
+    elif v10==6:
         print("Better not tell you now.")
-elif v11==7:
+    elif v10==7:
         print("My sources say no.")
-elif v11==8:
+    elif v10==8:
         print("Outlook not so good.")
-elif v11==9:
+    elif v10==9:
         print("Very doubtful.")
+    ask_again()
+
+def ask_again():    
+    x2 = input('Do you want to ask another question? Yes or no?')
+    v11 =random.randint(1,9)
+    if x2 == 'nah' or x2 == 'no' or x2 == 'n':
+        print('goodbye!')
+    elif x2 == 'yes' or x2 == 'y':
+        print('what is your question')
+        eight_ball_responses()
+    else:
+        print('have a good day!')
+ask_again()
